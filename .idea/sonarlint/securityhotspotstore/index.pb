d6944752e8dbb7723e7aff9ec247f0d03e0a723b
Jay <jp6631@srmist.edu.in>
2024-03-09 23:16:05 +0530

    creating video call page

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -7,3 +7,5 @@ D
 app/build.gradle.kts,d/2/d2a4383452aad84a18429a5844d31330a012a9e6
 e
 5app/src/main/java/com/example/chatapp/MainActivity.kt,a/1/a198ed466ffd9c7064194bf6c6f348be46c2c6b7
+j
+:app/src/main/java/com/example/chatapp/VideoCallActivity.kt,c/8/c8389073abf5cf8b0fd58dbd9ed0350cae0e0bda
--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.zegocloud.uikit.prebuilt.call.config.ZegoNotificationConfig
+import com.zegocloud.uikit.prebuilt.call.invite.ZegoUIKitPrebuiltCallInvitationConfig
+import com.zegocloud.uikit.prebuilt.call.invite.ZegoUIKitPrebuiltCallInvitationService
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userIdTextField : EditText
+    private lateinit var button : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userIdTextField = findViewById(R.id.user_id_text_field)
+        button = findViewById(R.id.button_next)
+
+        button.setOnClickListener {
+            val userId = userIdTextField.text.toString()
+            if(user)
+        }
+    }
+
+    private fun videocallServices(userID: String) {
+        val appID : Long = 931425084
+        val appSign = "589912f4b54cd6086466e4454f1f888d2d2e340f98ba9f8e45961c4a85c6f71f"
+        val application = application
+        val callInvitationConfig = ZegoUIKitPrebuiltCallInvitationConfig()
+        callInvitationConfig.notificationConfig = ZegoNotificationConfig()
+        val notificationConfig = ZegoNotificationConfig()
+        notificationConfig.sound = "zego_uikit_sound_call"
+        notificationConfig.channelID = "CallInvitation"
+        notificationConfig.channelName = "CallInvitation"
+        ZegoUIKitPrebuiltCallInvitationService.init(application, appID, appSign, userID, userID, callInvitationConfig,)
     }
 }
